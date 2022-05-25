84df4da5fd9c8be2b89354a84fd846efe8b6d606
Mathijs Bernson <mathijsb@q42.nl>
2022-05-25 14:32:08 +0200

    Initial Commit

---
+++ RefreshableScrollViewDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RefreshableScrollViewDemo
+//
+//  Created by Mathijs Bernson on 25/05/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RefreshableScrollViewDemo/RefreshableScrollViewDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RefreshableScrollViewDemoApp.swift
+//  RefreshableScrollViewDemo
+//
+//  Created by Mathijs Bernson on 25/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct RefreshableScrollViewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
